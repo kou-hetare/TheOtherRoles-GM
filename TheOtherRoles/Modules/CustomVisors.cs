545c4332da6cc2d449435e862e1404a1c8b36b2f
kou-hetare <96226646+kou-hetare@users.noreply.github.com>
2021-12-27 23:59:02 +0900

    AltShader実験中

--- TheOtherRoles/Modules/CustomVisors.cs
+++ TheOtherRoles/Modules/CustomVisors.cs
@@ -42,13 +42,13 @@ namespace TheOtherRoles.Modules
             Texture2D texture = fromDisk ? Helpers.loadTextureFromDisk(path) : Helpers.loadTextureFromResources(path);
             if (texture == null)
             {
-//                System.Console.WriteLine("Texture Error:"+path+"\n");
+                //                System.Console.WriteLine("Texture Error:"+path+"\n");
                 return null;
             }
             Sprite sprite = Sprite.Create(texture, new Rect(0, 0, texture.width, texture.height), new Vector2(0.53f, 0.575f), texture.width * 0.5f);
             if (sprite == null)
             {
-//                System.Console.WriteLine("Sprite Error\n");
+                //                System.Console.WriteLine("Sprite Error\n");
                 return null;
             }
             texture.hideFlags |= HideFlags.HideAndDontSave | HideFlags.DontUnloadUnusedAsset;
@@ -63,10 +63,10 @@ namespace TheOtherRoles.Modules
             }
             else
             {
-               Assembly assembly = Assembly.GetExecutingAssembly();
+                Assembly assembly = Assembly.GetExecutingAssembly();
                 try {
-                    var st=assembly.GetManifestResourceInfo(path);
-                    return st!=null;
+                    var st = assembly.GetManifestResourceInfo(path);
+                    return st != null;
                 }
                 catch
                 {
@@ -79,13 +79,13 @@ namespace TheOtherRoles.Modules
             VisorData visor = new VisorData();
             System.Console.WriteLine(cv.Idle + "\n");
             var ext = Path.GetExtension(cv.Idle);
-//            System.Console.WriteLine(ext + "\n");
+            //            System.Console.WriteLine(ext + "\n");
             var basename = cv.Idle.Substring(0, cv.Idle.Length - ext.Length);
-//            System.Console.WriteLine(basename + "\n");
+            //            System.Console.WriteLine(basename + "\n");
 
             visor.IdleFrame = CreateVisorSprite(cv.Idle, fromDisk);
             int order;
-            if (basename.Last()=='L')
+            if (basename.Last() == 'L')
             {
                 order = 99;
             }
@@ -98,21 +98,21 @@ namespace TheOtherRoles.Modules
                 order = 99;
             }
 
-            var IdleLeft = basename  + "_Left" + ext;
-//            System.Console.WriteLine(IdleLeft + "\n");
+            var IdleLeft = basename + "_Left" + ext;
+            //            System.Console.WriteLine(IdleLeft + "\n");
             if (existImage(IdleLeft, fromDisk))
             {
                 visor.LeftIdleFrame = CreateVisorSprite(IdleLeft, fromDisk);
 
             }
-//            visor.FloorFrame= CreateVisorSprite(cv.Floor, fromDisk);
+            //            visor.FloorFrame= CreateVisorSprite(cv.Floor, fromDisk);
             visor.name = cv.Name;
             visor.ProductId = cv.Id;
             visor.ChipOffset = new Vector2(0f, 0.0f);
             visor.Free = true;
             visor.NotInStore = true;
             visor.Order = order;
-            
+
             return visor;
         }
         private static List<customVisor> visorList;
@@ -162,6 +162,13 @@ namespace TheOtherRoles.Modules
 //                    Floor="TheOtherRoles.Resources.VisorTest.visorGundam1.png",
                     Name ="ZK",
                     Id   ="visorZaku",
+                    FromDisk=false
+                },
+                new customVisor(){
+                    Idle ="TheOtherRoles.Resources.VisorTest.VisorZakuA.png",
+//                    Floor="TheOtherRoles.Resources.VisorTest.visorGundam1.png",
+                    Name ="ZK(カラー)",
+                    Id   ="visorZakuA",
                     FromDisk=false
                 },
                 new customVisor(){
@@ -251,14 +258,16 @@ namespace TheOtherRoles.Modules
 
         }
 
-        [HarmonyPatch(typeof(HatManager), nameof(HatManager.GetVisorById))]
-        private static class HatManagerPatch
+        [HarmonyPatch(typeof(HatManager), nameof(HatManager.GetVisorById), typeof(string))]
+        public static class HatManagerPatch
         {
             private static bool LOADED;
             private static bool RUNNING;
 
-            static void Prefix(HatManager __instance)
+            static void Prefix(string visorId, HatManager __instance)
             {
+                FileLog.Log("Prefix HatManager.GetVisorById");
+                FileLog.Log(" Visor:" + visorId);
                 if (RUNNING) return;
                 RUNNING = true; // prevent simultanious execution
 
@@ -266,20 +275,21 @@ namespace TheOtherRoles.Modules
                 {
                     if (!LOADED)
                     {
-                        while (visorList.Count!=0)
+                        FileLog.Log(" Visor Loading");
+                        while (visorList.Count != 0)
                         {
                             var cv = visorList[0];
                             visorList.RemoveAt(0);
-//                            UnityEngine.Debug.Log("visor Add:" + cv.ToString() + "\n");
-                            __instance.AllVisors.Add(CreateVisorData(cv,cv.FromDisk));
+                            //                            UnityEngine.Debug.Log("visor Add:" + cv.ToString() + "\n");
+                            __instance.AllVisors.Add(CreateVisorData(cv, cv.FromDisk));
                         }
                     }
                 }
-                catch (System.Exception e)
+                catch (System.Exception)
                 {
                     if (!LOADED)
                     {
-//                        UnityEngine.Debug.Log("Unable to add Custom Hats\n" + e);
+                        //                        UnityEngine.Debug.Log("Unable to add Custom Hats\n" + e);
 
                     }
                 }
@@ -291,34 +301,208 @@ namespace TheOtherRoles.Modules
             }
         }
     }
-/*
-    [HarmonyPatch(typeof(VisorLayer), nameof(VisorLayer.SetVisor),typeof(VisorData))]
-    public class VisorLayerSetVisorPatch
-    {
-        private static Material hatShader;
-
-        public static void Postfix(VisorLayer __instance)
+    /*
+        [HarmonyPatch(typeof(HatManager.__c__DisplayClass16_0), nameof(HatManager.__c__DisplayClass16_0._GetVisorById_b__0), typeof(VisorData))]
+        public static class HatManagerDC16Patch
         {
-            if (hatShader == null && DestroyableSingleton<HatManager>.InstanceExists)
+            static void Prefix(VisorData v, HatManager.__c__DisplayClass16_0 __instance)
             {
-                UnityEngine.Debug.Log("Serch AltShader\n");
+                FileLog.Log("Prefix HatManager.__c__DisplayClass16_0.GetVisorById Visor:" + v.ProductId);
+            }
+        }
+    */
+
+    //*
+    [HarmonyPatch(typeof(VisorsTab))]
+    public class VisorsTabPatch
+    {
+        //*
+        [HarmonyPostfix]
+        [HarmonyPatch(nameof(VisorsTab.SelectVisor), new System.Type[] { typeof(ColorChip), typeof(VisorData) })]
+        public static void SelectVisorPath(ColorChip chip, VisorData visor, VisorsTab __instance)
+        {
+            FileLog.Log("Pre.VisorsTab.SelectVisor");
+            if (visor!=null)
+            {
+                FileLog.Log(" Visor: " + visor?.ProductId);
+            }
+            else
+            {
+                FileLog.Log(" Visor: null");
+            }
+            if (VisorLayerPatch.altShader != null)
+            {
+                if (visor.ProductId.Last() == 'A')
+                {
+                    FileLog.Log(" visor Shader Changed");
+                    __instance.PlayerPreview.VisorSlot.Image.material = VisorLayerPatch.altShader;
+                    //                    int colorID = PlayerControl.LocalPlayer.CurrentOutfit.ColorId;
+                    //                    Color bodycolor = Palette.PlayerColors[colorID];
+                    //                    Color shadowcolor = Palette.ShadowColors[colorID];
+                    //FileLog.Log(" colorID:" + colorID);
+                    Color bodycolor = __instance.PlayerPreview.Body.material.GetColor(2);
+                    Color shadowcolor = __instance.PlayerPreview.Body.material.GetColor(1);
+                    FileLog.Log(" Body Color:" + bodycolor.ToString());
+                    __instance.PlayerPreview.VisorSlot.Image.material.SetColor(2, bodycolor);
+                    __instance.PlayerPreview.VisorSlot.Image.material.SetColor(1,shadowcolor);
+                    FileLog.Log(" altShader:" + VisorLayerPatch.altShader);
+                    //                    FileLog.Log(" BodyColor:" + __instance.PlayerPreview.Body.color.ToString());
+                }
+                else
+                {
+                    FileLog.Log(" visor Shader Original");
+                    __instance.PlayerPreview.VisorSlot.Image.material = VisorLayerPatch.orgShader;
+                    //                    __instance.PlayerPreview.VisorSlot.Image.color = VisorLayerSetVisorPatch.orgColor;
+
+                }
+            }
+        }
+        //*/
+    }
+    /*
+    [HarmonyPatch(typeof(HatParent))]
+    public static class HatParentPatch
+    {
+        [HarmonyPatch(nameof(HatParent.FrontLayer), MethodType.Setter)]
+        static void Prefix(SpriteRenderer value, HatParent __instance)
+        {
+            FileLog.Log("HatParent.FrontLayer Setter");
+
+        }
+    }
+    */
+    /*
+        [HarmonyPatch(typeof(ColorChip))]
+        public static class ColorChiptPatch
+        {
+            [HarmonyPrefix]
+            [HarmonyPatch(nameof(ColorChip.Inner), MethodType.Setter)]
+            static void Prefix(ColorChip __instance, HatParent value)
+            {
+                FileLog.Log("ColorChip.Inner Setter");
+                FileLog.Log(" HatParentType:" + value.GetType().ToString());
+            }
+        }
+    */
+    [HarmonyPatch(typeof(VisorLayer))]
+    public class VisorLayerPatch
+    {
+        public static Material orgShader;
+        public static Color orgColor;
+        public static Material altShader;
+
+        [HarmonyPostfix]
+        [HarmonyPatch(nameof(VisorLayer.SetVisor), typeof(VisorData))]
+        public static void fix_SetVisor_VisorData(VisorData data,VisorLayer __instance)
+        {
+            FileLog.Log("post.VisorLayer.SetVisor(VisorData)");
+            FileLog.Log(" Visor:" + data.ProductId);
+            UnityEngine.Debug.Log("VisorLayer.SetVisor:" + data.ProductId);
+            //           UnityEngine.Debug.Log(" org_z:" + org_z);
+            UnityEngine.Debug.Log(" localpos.z:" + __instance.transform.localPosition.z);
+
+            /*
+                        if (org_z == 0.0f)
+                        {
+                            UnityEngine.Debug.Log(" Set org_z");
+                            org_z = __instance.transform.position.z;
+                        }
+                        if (data.ProductId.Last() == 'L')
+                        {
+                            var newpos = __instance.transform.localPosition;
+                            newpos.z = -0.0001f;
+                            __instance.transform.localPosition = newpos;
+                        }
+                        else
+                        {
+                            var newpos = __instance.transform.localPosition;
+                            newpos.z = -0.0002f;
+                            __instance.transform.localPosition = newpos;
+
+                        }
+            */
+            if (orgColor == null) orgColor = __instance.Image.color;
+            if (orgShader == null) orgShader = __instance.Image.material;
+            if (altShader == null)
+            {
+                if (DestroyableSingleton<HatManager>.InstanceExists)
+                {
+                    FileLog.Log(" Serch AltShader");
+                    foreach (HatBehaviour h in DestroyableSingleton<HatManager>.Instance.AllHats)
+                    {
+                        if (h.AltShader != null)
+                        {
+                            FileLog.Log(" AltShader found");
+                            altShader = h.AltShader;
+                            break;
+                        }
+                    }
+                }
+            }
+            FileLog.Log(" orgColor:" + orgColor.ToString());
+            FileLog.Log(" orgShader:" + orgShader.ToString());
+            FileLog.Log(" altShader:" + altShader.ToString());
+
+            /*
+            if (altShader!=null)
+            {
+                if (data.ProductId.Last() == 'A' )
+                {
+                    FileLog.Log(" visor Shader Changed\n");
+                    __instance.Image.material = altShader;
+                    __instance.Image.color = Palette.PlayerColors[7];
+
+                }
+                else
+                {
+                    FileLog.Log(" visor Original Shader");
+                    __instance.Image.material = orgShader;
+                    __instance.Image.color = orgColor;
+                }
+            }
+            //*/
+        }
+
+        [HarmonyPostfix]
+        [HarmonyPatch(nameof(VisorLayer.SetVisor), typeof(string))]
+        public static void fix_SetVisor_string(string visorId, VisorLayer __instance)
+        {
+            FileLog.Log("post.VisorLayer.SetVisor(string)");
+            FileLog.Log(" Visor:" + visorId);
+            /*
+            if (orgColor == null) orgColor = __instance.Image.color;
+            if (orgShader == null) orgShader = __instance.Image.material;
+            if (altShader == null && DestroyableSingleton<HatManager>.InstanceExists)
+            {
+                FileLog.Log(" Serch AltShader");
                 foreach (HatBehaviour h in DestroyableSingleton<HatManager>.Instance.AllHats)
                 {
                     if (h.AltShader != null)
                     {
-                        UnityEngine.Debug.Log(" AltShader found\n");
-                        hatShader = h.AltShader;
+                        FileLog.Log(" AltShader found");
+                        altShader = h.AltShader;
                         break;
                     }
                 }
             }
-            if (hatShader != null)
-            {
-                UnityEngine.Debug.Log("visor Shader Changed\n");
-                __instance.Image.material = hatShader;
 
+            if (altShader!=null)
+            {
+                if (visorId.Last() == 'A')
+                {
+                    FileLog.Log(" visor Shader Changed\n");
+                    __instance.Image.material = altShader;
+                    __instance.Image.color = Palette.PlayerColors[7];
+
+                }
+                else
+                {
+                    FileLog.Log(" visor Original Shader");
+                    __instance.Image.material = orgShader;
+                    __instance.Image.color = orgColor;
+                }
             }
+            */
         }
     }
-*/
 }
